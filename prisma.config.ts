dc02bf62fdcf798588b17d512aff3a1a5db7c854
Eduard Abajyan <edwardabajyan@gmail.com>
2026-03-19 18:22:58 +0400

    Authentication worked !

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "prisma/config";
+import { config } from "dotenv";
+
+// Manually load your Next.js environment variables
+config({ path: ".env.local" });
+config({ path: ".env" });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL, // ✅ top-level
-    shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL, // optional
+    url: process.env.DATABASE_URL!,
+
+    // @ts-expect-error directUrl is supported by the engine but missing from v7 types
+    directUrl: process.env.DIRECT_URL!,
   },
 });
